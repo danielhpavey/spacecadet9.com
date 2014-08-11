022afc99bccc18503e40eb13ea44f57fbdc18baa
Daniel H Pavey <danielhpavey@gmail.com>
2014-08-11 22:26:16 +0100

    Added a category list to search page

--- wp-content/themes/spacecadet9/searchpage.php
+++ wp-content/themes/spacecadet9/searchpage.php
@@ -10,6 +10,21 @@ Template Name: Search Page
 		<h2 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'twentyeleven' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a>
 		</h2>
 		<?php get_search_form(); ?>
+
+
+<br />
+<br />
+<h2>Categories</h2>
+ <?php 
+
+$args = array(
+        show_count=>1
+        ,title_li =>''
+        );
+
+wp_list_categories( $args ); ?> 
+
+
 	</article>
 </section>
 <?php get_footer(); ?>
